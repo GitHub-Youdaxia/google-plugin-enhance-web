033df6275cc166261a4af9514d9cea17e923187a
GitHub-Youdaxia <2354339265@qq.com>
2025-07-26 00:36:56 +0800

    Add files via upload

---
+++ webScript/pulicScript/main.js
@@ -0,0 +1,10 @@
+/*
+var mainComponentUrl111 = document.querySelector('#aaaaaaaa').getAttribute('url')
+new Vue({
+  el: '#aaaaaaaa',
+  components: {
+    'my-component': httpVueLoader(mainComponentUrl111)
+  },
+  template: '<my-component></my-component>'
+})
+ */
