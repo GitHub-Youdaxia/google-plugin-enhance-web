6274942bfa3d4db52d35ec4a243d7205ed6e34f9
youhanghang <2354339265@qq.com>
2025-07-26 09:05:57 +0800

    1

--- webScript/pulicScript/main.js
+++ webScript/pulicScript/main.js
@@ -1,8 +1,12 @@
-const mainComponentUrl111 = document.querySelector('#aaaaaaaa').getAttribute('url')
+(()=>{
+let baseUrl = `https://github-youdaxia.github.io/google-plugin-enhance-web/webScript`
+
 new Vue({
   el: '#aaaaaaaa',
   components: {
-    'my-component': httpVueLoader(mainComponentUrl111)
+    'my-component': httpVueLoader(`${baseUrl}/pulicScript/components/main.vue`)
   },
   template: '<my-component></my-component>'
 })
+
+})()
